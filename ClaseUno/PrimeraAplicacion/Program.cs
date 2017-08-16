15282d1044f557943e2b066a66c99fe792a358fe
AyalaProgramas <31025807+AyalaProgramas@users.noreply.github.com>
2017-08-16 12:02:39 -0300

    Add files via upload

---
+++ ClaseUno/PrimeraAplicacion/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            //Console.WriteLine("hola mundo");
+            //Console.ReadKey();
+
+            int numeroUno;
+            int numeroDos;
+            int Resultado;
+            string dato;
+            bool esnumero;
+            int cont = 0;
+            bool flaps;
+
+            //dato = Console.ReadLine();
+            //esnumero = int.TryParse(Console.ReadLine(), out numeroUno);
+
+            while (!int.TryParse(Console.ReadLine(), out numeroUno))
+            {
+                Console.WriteLine("error ingrese, el numero uno");
+                cont++;
+                if(cont>5)
+                {
+
+                }
+            }
+
+            //dato = Console.ReadLine();
+            //numeroUno = int.Parse(dato);
+            //dato = Console.ReadLine();
+            //numeroDos = int.Parse(dato);
+
+
+
+            //numeroDos = 10;
+            //numeroUno = 12;
+
+           // Resultado = numeroDos + numeroUno;
+
+           //Console.Write("el resultado es:"+Resultado);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ClaseUno/ejercicioDos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioDos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno;
+            //int numeroDos;
+            //int Resultado;
+            int numeromayor=0;
+            int numeromenor=0;
+            string dato;
+            bool esnumero;
+            int contador = 0;
+            
+            while(contador<5)
+            {
+                
+                while(!int.TryParse(Console.ReadLine(), out numeroUno))
+                {
+                    Console.WriteLine("error ingrese, el numero uno");
+                }
+
+
+                if(contador==0)
+                {
+                    numeromayor=numeroUno;
+                    numeromenor=numeroUno;
+                }
+                else
+                {
+                    if(numeromayor<numeromenor)
+                    {
+
+                    }
+                }
+
+                contador++;
+            }
+             
+               
+            //while (!int.TryParse(Console.ReadLine(), out numeroUno))
+            //{
+            //    
+                //cont++;
+                //if(cont>5)
+                //{
+                //    flaps=true;
+                //}
+            }
+            //Console.WriteLine("error ingrese, el numero uno");
+            //cont++;
+            //if (cont > 5)
+            //{
+            //    flaps = true;
+            //}
+        }
+    }
+}
