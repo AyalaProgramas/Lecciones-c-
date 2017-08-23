cb6751894088815e12f9dfb17bb60bfd980329df
AyalaProgramas <31025807+AyalaProgramas@users.noreply.github.com>
2017-08-23 12:01:54 -0300

    Add files via upload

---
+++ clasecsharpnum3/clasecsharpnum3/Auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasecsharpnum3
+{
+    class Auto
+    {
+        public string marca;//si no escribo public por defecto es private y por la tanto no lo veo
+        public string patente;
+        public float precio;
+        public static int cantinstancia=0;//si pongo cantinstancia=0 siempre va tener ese valor q es 0
+        public static DateTime f_inicio;
+        public static DateTime f_final;
+        public static DateTime cantdetiempo;
+
+        public Auto()
+        {
+            //if (cantinstancia == 0)
+            //{
+            //    Auto.f_inicio = DateTime.Now;
+            //}
+            Auto.cantinstancia++;//es un atributo de clase
+            this.marca = "sin marca";
+        }
+
+        static Auto()
+        {
+            Auto.f_inicio = DateTime.Now;
+        }
+
+
+        /*public*/private string mostrar()
+        {
+            return this.marca+this.patente+this.precio;
+
+
+            //Console.WriteLine(marca);
+            //Console.WriteLine(patente);
+            //Console.WriteLine(precio);
+        }
+
+        public static string mostrarmetodoprivate(Auto autouno)//llama al metodo private mostrar
+        {
+            //return autouno.marca + autouno.patente + autouno.precio;
+            return autouno.mostrar();//reutiliso el metodode arriba
+
+            //Console.WriteLine(marca);
+            //Console.WriteLine(patente);
+            //Console.WriteLine(precio);
+        }
+    }
+}
---
+++ clasecsharpnum3/clasecsharpnum3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasecsharpnum3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto();
+            Auto a2 = new Auto();
+
+            a1.marca = "fiat";
+            a1.patente = "A001";
+            a1.precio = 130000;
+
+            a2.marca = "ford";
+            a2.patente = "B00Z";
+            a2.precio = 80000;
+
+            //Console.Write(a1.mostrar());
+            //Console.Write(a2.mostrar());
+
+            a1 = a2;
+
+            Console.WriteLine(Auto.mostrarmetodoprivate(a1));
+            Console.WriteLine(Auto.mostrarmetodoprivate(a2));
+
+            //Auto.f_final = DateTime.Now;
+
+        }
+    }
+}
